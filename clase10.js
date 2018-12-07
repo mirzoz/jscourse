9d30ef6b50c1dd5225fdd7f3c2eee158ef5f18b5
Marco Tzuc <mirzoz12@gmail.com>
2018-12-06 20:18:01 -0600

    class110

---
+++ clase10.js
@@ -0,0 +1,43 @@
+var man = {
+  name: 'marco',
+  lastName: 'tuzo',
+  age: 28,
+  engineer: true,
+  chef: false,
+  singer: false,
+  dj: false,
+  guitarrist: false,
+  drone: true
+};
+
+
+function printProfessions(man) {
+  console.log(`${man.name} es:`)
+
+  if (man.engineer === true) {
+    console.log(`Engineer`)
+  } else console.log(`No es`)
+
+  if (man.chef === true) {
+    console.log(`chef`)};
+
+  if (man.drone === true) {
+    console.log(`pilot`)};
+
+  if (man.guitarrist === true) {
+    console.log(`Engineer`)};
+};
+
+
+printProfessions(man)
+
+
+function printLegalAge(man) {
+  console.log(`${man.name} es:`)
+
+  if (man.age >= 18) {
+    console.log(`${man.name} mayor de edad`)
+  } else console.log(`${man.name} menor de edad`)
+}
+
+printLegalAge(man)
